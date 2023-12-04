5d54c29f5474c15cf01e0b732765872c1794e073
correantonio <antonioalb97@gmail.com>
2023-12-03 22:04:17 -0300

    [BUG] Tentativa de adicionar o nome dos alunos ao hover do mouse

---
+++ script.js
@@ -0,0 +1,16 @@
+// const alunosImgs = document.querySelectorAll('.lista-alunos .aluno-img')
+// const alunoNomes = document.querySelectorAll('.lista-alunos .aluno-nome')
+
+// alunosImgs.forEach((img, imgIndex)=>{
+//   img.addEventListener('mouseover', ()=>{
+//     alunoNomes.forEach((nome, nomeIndex) =>{
+//       if(imgIndex == nomeIndex){
+//         if(nome.classList.contains('aluno-nome--inactive')){
+//           nome.classList.remove('aluno-nome--inactive')
+//         }else{
+//           nome.classList.add('aluno-nome--inactive')
+//         }
+//       }
+//     })
+//   })
+// })
